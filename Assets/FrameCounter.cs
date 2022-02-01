3111f1e63f716337897cba6a0f4efa1c85dd91be
janick3110 <janick3110@gmail.com>
2022-02-01 23:26:02 +0100

    Frame Counter

---
+++ Assets/FrameCounter.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrameCounter : MonoBehaviour
+{
+    private Text displayFrames;
+
+    private void Start()
+    {
+        displayFrames = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        float current = (int)(1f / Time.unscaledDeltaTime);
+        displayFrames.text = "FPS: "  + current.ToString();
+    }
+}
